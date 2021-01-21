1633a03f9ee3a375897e037dad840ef5d788a150
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2021-01-21 08:41:48 -0800

    Organizing

---
+++ Coding/Day1/Questions1.py
@@ -0,0 +1,16 @@
+# Coding Exercises:
+# 1.	Ask the user for first name(Ada), second name (Lovelace), age in years (10), height (5.5) in feet and weight(152) in lbs. Print the output as follows:
+# Good day Ada Lovelace !!
+# You have lived at least 120 months or 520 weeks or 3650 days.
+# Your height is 5.5 feet or 66 inch or 167.64 cms.
+# Your weight is 152 lbs or 68.95 kg.
+# Nice to meet you. Bye
+# 2.	Given the length in miles => convert it to inch, feet, yard, km, m, cm
+# 3.	Given the number of days => convert it to hours, minutes and seconds, weeks.
+# 4.	Give the dollar amount => tell me how many pennies, nickels, dimes, quarters, half dollars you need.
+# For 1.41 dollars:
+# I need 2 half dollars , 1 quarter, 1 dime, 1 nickel and 1 penny.
+# Also 141 pennies = 70 nickels + 1 cent = 14 dimes + 1cent = 5 quarters + 16 cents = 2 half dollars + 41 cents.
+# 5.	Given the mass in lbs => convert to ounces, g and kg
+# 6.	Given the volume in quarts => convert to cups, pints, gallons, ml, l
+# 7.	Given the temperature in Fahrenheit convert to Celsius c and Kelvin
