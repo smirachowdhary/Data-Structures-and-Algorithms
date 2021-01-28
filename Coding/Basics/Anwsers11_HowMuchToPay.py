ccdc9f7b0c89178ccab4be3b31a096896ea78cee
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2021-01-28 10:27:27 -0800

    Given a wall with length l1 and width w1 that has a widow of dimension length l2 and width w2 and cost per gallon is $20 and each gallon paints 3 meter square. Generate a receipt how much the customer should pay for the paint.

---
+++ Coding/Basics/Anwsers11_HowMuchToPay.py
@@ -0,0 +1,18 @@
+name = input("Enter Name:")
+length_of_wall = float(input("Enter length of wall in meters:"))
+widith_of_wall = float(input("Enter widith of wall in meters:"))
+length_of_window = float(input("Enter length of window  in meters:"))
+widith_of_window = float(input("Enter widith of window in meters:"))
+perimeter_of_wall = length_of_wall + length_of_wall + widith_of_wall
+perimeter_of_window = length_of_window + length_of_window + widith_of_window + widith_of_window
+wall_that_is_not_window = perimeter_of_wall - perimeter_of_window
+gallons_of_paint = wall_that_is_not_window / 3
+cost = gallons_of_paint * 20
+
+print(f"""
+{name}
+You have bought:
+{gallons_of_paint} gallons of paint.
+The cost is:
+${cost}
+""")
