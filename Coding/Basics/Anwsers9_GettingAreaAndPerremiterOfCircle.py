efa3819bcc786688de723a8bbc9cb20c36207277
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2021-01-28 09:32:14 -0800

    Calculate the perimeter and area of circle given radius

---
+++ Coding/Basics/Anwsers9_GettingAreaAndPerremiterOfCircle.py
@@ -0,0 +1,11 @@
+radius = float(input("Enter raduis of circle:"))
+area = radius * 2 * 3.14
+diameter = 2 * radius
+circumference = 3.14 * diameter
+
+print(f"""
+Radius: {}
+Area: {}
+Diameter: {}
+Circumstance (preimeter of circle): {}
+""")
