5c7cc2903cb66687ca28e18da4e270bf70ded77a
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2021-01-28 08:49:45 -0800

    Update Question 1 such that Name is displayed in uppercase

--- Coding/Basics/L0001_Anwsers1_UserInfo.py
+++ Coding/Basics/L0001_Anwsers1_UserInfo.py
@@ -7,8 +7,8 @@ height_in_feet = float(input("How tall are you (in feet)?"))
 weight_in_pounds = float(input("How much do you weigh (in pounds)?"))
 
 print(f""" 
-Good day, {first_name} {last_name}!!
-You have lived at least {age_in_years:5.0f} years, {age_in_years * 12:5.0f} months, {age_in_years * 52.1429:5.0f} weeks, or {age_in_years * 365:5.0f} days.
+Good day, {first_name.upper()} {last_name.upper()}!!
+You have lived at least {age_in_years:2.0f} years, {age_in_years * 12:2.0f} months, {age_in_years * 52.1429:5.0f} weeks, or {age_in_years * 365:2.0f} days.
 Your height is {height_in_feet} feet, {height_in_feet * 12:5.3f} inches, or {height_in_feet * 30.48:5.3f} centimeters.
 Your weight is {weight_in_pounds:5.2f} pounds or {weight_in_pounds / 2.205:5.4f} kilograms.
 Nice to meet you. Bye!
