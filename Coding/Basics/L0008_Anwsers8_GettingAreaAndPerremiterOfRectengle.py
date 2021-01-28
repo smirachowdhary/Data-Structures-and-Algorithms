96bc7ff70910f4d2287a7771b20fd21b2bf71145
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2021-01-28 09:11:22 -0800

    better file names and deleting copy

--- Coding/Basics/L0008_Anwsers8_GettingAreaAndPerremiterOfRectengle.py
+++
@@ -1,8 +0,0 @@
-length = float(input("Enter length of rectengle:"))
-widith = float(input("Enter widith of rectengle:"))
-length_times_two = length * 2
-widith_times_two = widith * 2
-perimeter = length_times_two + widith_times_two
-area = length * widith
-
-print(f"Perimeter of your rectengle is: {perimeter}. Area of your rectengle is: {area}.")
