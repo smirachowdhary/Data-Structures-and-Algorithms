ec8050f29a99d16fcf892cf485fb68d0942b1798
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2021-02-10 11:16:03 -0800

    Reorganized

--- Basics/Anwsers9_GettingAreaAndPerremiterOfSquare.py
+++
@@ -1,5 +0,0 @@
-length = float(input("Enter length of square:"))
-perimeter = length * 4
-area = length * length
-
-print(f"Perimeter of your square is: {perimeter}. Area of your square is: {area}.")
