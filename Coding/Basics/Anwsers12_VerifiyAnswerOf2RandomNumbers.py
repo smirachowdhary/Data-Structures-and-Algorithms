b2d239bbf58bd961d316a32bdf37818d98c91fbe
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2021-02-10 10:56:01 -0800

    Extend Q 12 to give user 20 questions and keep a score. Give user one retry on failure.

--- Coding/Basics/Anwsers12_VerifiyAnswerOf2RandomNumbers.py
+++ Coding/Basics/Anwsers12_VerifiyAnswerOf2RandomNumbers.py
@@ -2,15 +2,24 @@ import random
 
 random_number1 = random.randint(100,1000)
 random_number2 = random.randint(1,100)
+wrong = 0
+right = 0
 
-print(f"{random_number1} * {random_number2}")
-User_answer = int(input("Enter Answer(No commas or you'll get your question wrong):"))
-if random_number1 * random_number2 == User_answer:
-    print("You are correct!!")
-else:
-    print("Sorry, you are wrong. Try again(this is your last chance).")
-    Last_chance = int(input("Enter Answer(No commas or you'll get your question wrong):"))
-    if random_number1 * random_number2 == Last_chance:
+i = 0
+while i > 20:
+    print(f"{random_number1} * {random_number2}")
+    User_answer = int(input("Enter Answer(No commas or you'll get your question wrong):"))
+    if random_number1 * random_number2 == User_answer:
         print("You are correct!!")
+        right+=1
     else:
-        print(f"Sorry, you are wrong. The real answer is {random_number1 * random_number2}.")
+        print("Sorry, you are wrong. Try again(this is your last chance).")
+        Last_chance = int(input("Enter Answer(No commas or you'll get your question wrong):"))
+        if random_number1 * random_number2 == Last_chance:
+            print("You are correct!!")
+            right+=1
+        else:
+            print(f"Sorry, you are wrong. The real answer is {random_number1 * random_number2}.")
+            wrong+=1
+    i+=1
+print(f"Out of 20 questions, you got {wrong} wrong and {right} right.")
