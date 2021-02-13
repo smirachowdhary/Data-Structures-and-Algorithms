807554e21489e90dc2e1841c745a5d7ff34f5738
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2021-02-13 13:41:11 -0800

    some useful while loops

---
+++ Repetations/Answers17_WhilePractice.py
@@ -0,0 +1,36 @@
+n = int(input("Give me a number:"))
+# Printing 1 to n
+i = 0
+while i < n:
+    print(i+1, end=" ")
+    i+=1
+print("\n")
+# Printing 0 to n-1
+i = 0
+while i < n:
+    print(i, end=" ")
+    i+=1
+print("\n")
+# Printing n-1 to 0
+i = n-1
+while i < n:
+    print(i, end=" ")
+    i-=1
+print("\n")
+# Printing n to 1
+i = 0
+while i < n:
+    print(n-i, end=" ")
+    i+=1
+print("\n")
+# Printing n odd numbers, starting from 1
+i = 0
+while i < n:
+    print(2*i+1, end=" ")
+    i+=1
+print("\n")
+# Printing n even numbers, starting from 0
+i = 0
+while i < n:
+    print(2*i, end=" ")
+    i+=1
