c7c23e94073d33c4296f00233153fd99f13a6522
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2021-02-23 08:09:41 -0800

    Finding max of 3 numbers

--- Functions/Answers26_FindMaxOf3Numbers.py
+++ Functions/Answers26_FindMaxOf3Numbers.py
@@ -1,22 +1,28 @@
 import random
 
-def maximum(n1, n2, n3)
-    max = n1
-    if n1 < n2:
-        max = n2
-        if n2 < n3:
-            max = n3
-            print(f"The max of the 3 numbers is {max}.")
-        elif n2 > n3:
-            print(f"The max of the 3 numbers is {max}.")
-    elif n1 < n3:
-        max = n3
-        print(f"The max of the 3 numbers is {max}.")
-    elif n1 > n3:
-        print(f"The max of the 3 numbers is {max}.")
-    elif n1 == n2 == n3:
+def maximum(a, b, c):
+    maxi = a
+    if a < b:
+        maxi = b
+        if b < c:
+            maxi = c
+            print(f"The max of the 3 numbers is {maxi}.")
+        elif b > c:
+            print(f"The max of the 3 numbers is {maxi}.")
+    elif a < c:
+        maxi = c
+        print(f"The max of the 3 numbers is {maxi}.")
+    elif a > c:
+        print(f"The max of the 3 numbers is {maxi}.")
+    elif a == b == c:
         print("All your numbers are equal to each other.")
 
 n1_from_user = int(input("Enter a number:"))
 n2_from_user = int(input("Enter a number:"))
 n3_from_user = int(input("Enter a number:"))
+def_from_user = maximum(n1_from_user, n2_from_user, n3_from_user)
+
+n1_from_random = random.randint(0,1000)
+n2_from_random = random.randint(0,1000)
+n3_from_random = random.randint(0,1000)
+def_from_random = maximum(n1_from_random, n2_from_random, n3_from_random)
