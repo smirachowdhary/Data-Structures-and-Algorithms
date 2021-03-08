de4e402696acd1138c6bfd05f310c78acd7cc6aa
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2021-03-08 12:14:05 -0800

    just doing it with both for and while

--- Functions/Answers29_FuctionsPractice.py
+++ Functions/Answers29_FuctionsPractice.py
@@ -1,9 +1,31 @@
-def is_prime(n):
-    end = n//2
-    for i in range(2, end+1):
-        if n % i == 0:
-            return "Number is prime"
-    return "Number is composite"
+is_or_is_not_prime = []
 
-print(is_prime(21))
-print(is_prime(23))
+def is_prime(n):
+    i=2
+    while i < n:
+        end = i//2
+        j=2
+        while j <= end:
+            if i % j == 0:
+                break
+            j+=1
+        else:
+            is_or_is_not_prime.append(i)
+        i+=1
+
+is_prime(8)
+print(is_or_is_not_prime)
+
+is_or_is_not_prime_2 = []
+
+def is_prime_2(n):
+    for i in range(2,n):
+        end=i//2
+        for j in range(2, end+1):
+            if i%j == 0:
+                break
+        else:
+            is_or_is_not_prime_2.append(i)
+
+print(is_prime_2(8))
+print(is_or_is_not_prime_2)
