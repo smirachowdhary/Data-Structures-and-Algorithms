b40e8e9127d38015510c8cb8b2a7193996773348
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2021-03-10 10:04:53 -0800

    GUI prigramming

---
+++ Examples/tmp.py
@@ -0,0 +1,17 @@
+import f1
+import c1
+
+# print(f1.is_prime(10))
+# print(f1.is_prime(11))
+# pl = f1.get_prime_list(130)
+# print(pl)
+# print(len(pl))
+
+obj1 = c1.My_Number(11)
+print(obj1.is_prime())
+print(obj1.is_even())
+
+obj2 = c1.My_Number(21)
+print(obj2.is_prime())
+print(obj2.is_even())
+
