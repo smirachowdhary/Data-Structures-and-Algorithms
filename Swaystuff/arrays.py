c8c9963f7323c78400b53e07bd0fb94a1beb2e57
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2022-03-11 10:21:58 -0800

    array example

---
+++ Swaystuff/arrays.py
@@ -0,0 +1,12 @@
+array = ["kotlc", "Hunger games", "Land of Stories"]
+# defining a varible
+
+array[1] = "5 Worlds"
+# changing one value in an array
+
+array.insert(3, "Wings of Fire")
+# adding a value to the array - value counting starts at zero
+
+print(f"My fav series are {array[0]}, {array[1]}, {array[2]}, and {array[3]}.")
+
+# Output: My fav series are kotlc, 5 Worlds, Land of Stories, and Wings of Fire.
