4f5f098da0aab96c519f2193bd686585327d8f76
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2022-03-11 10:53:33 -0800

    loop stuff

---
+++ Swaystuff/loops.py
@@ -0,0 +1,18 @@
+# i = 0
+# while i<5:
+#     print("Yay!")
+#     i+=1
+
+# print("It didn't work!!")
+
+book_series = ["kotlc", "Hunger games", "Land of Stories"]
+i = 0
+while i < len(book_series):
+    print(f"Yay! {i}")
+    i+=1
+
+for x in book_series:
+    print(f"Yay! {x}")
+    
+
+print("It didn't work!!")
