82c552c3a003d3728b8195a2ef53047c6b09c412
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2022-04-07 10:11:28 -0700

    more testcases

--- Leetcode/fizzbuzz for loop version.py
+++ Leetcode/fizzbuzz for loop version.py
@@ -1,23 +1,27 @@
 def fizzBuzz(n):
     if n < 1  or n > 10**4:
-            return f"{n} is greater then or equal to 10,000."
+            raise Exception("Invalid Value")
     
     fizzbuzz = []
     
-    for i in range(n):
-        if (i+1)%15 == 0:
+    for i in range(1,n+1):
+        if (i)%15 == 0:
             fizzbuzz.append("FizzBuzz")
-        elif (i+1)%5 == 0:
+        elif (i)%5 == 0:
             fizzbuzz.append("Buzz")
-        elif (i+1)%3 == 0:
+        elif (i)%3 == 0:
             fizzbuzz.append("Fizz")
         else:
-            fizzbuzz.append(f"{i+1}")
+            fizzbuzz.append(f"{i}")
         
     return fizzbuzz
     
     #Space complexity = O(n)
     #Time complexity = O(n)
 
-testcase = fizzBuzz(15)
+testcase = fizzBuzz(150)
 print(testcase)
+# testcase2 = fizzBuzz(0)
+# print(testcase2)
+# testcase3 = fizzBuzz(10**4+10)
+# print(testcase3)
